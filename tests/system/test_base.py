2ba43961eb21938c0b7451cdd3fed02e6e7a42d3
claymoret <claymoret@163.com>
2018-01-17 01:37:26 +0800

    make setup

---
+++ tests/system/test_base.py
@@ -0,0 +1,19 @@
+from flumebeat import BaseTest
+
+import os
+
+
+class Test(BaseTest):
+
+    def test_base(self):
+        """
+        Basic test with exiting Flumebeat normally
+        """
+        self.render_config_template(
+            path=os.path.abspath(self.working_dir) + "/log/*"
+        )
+
+        flumebeat_proc = self.start_beat()
+        self.wait_until(lambda: self.log_contains("flumebeat is running"))
+        exit_code = flumebeat_proc.kill_and_wait()
+        assert exit_code == 0
